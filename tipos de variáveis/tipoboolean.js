1925f4b3a33f3ef51c5f84c93904a7d20a1d2014
loryovrx <126881218+loryovrx@users.noreply.github.com>
2023-10-11 09:52:30 -0300

    Add files via upload

---
+++ tipos de variáveis/tipoboolean.js
@@ -0,0 +1,8 @@
+//boolen
+//true
+
+const primeiroNumero = 5;
+const segundoNumero= 5;
+
+console.log(primeiroNumero === segundoNumero);
+
---
+++ tipos de variáveis/tipostring.js
@@ -0,0 +1,14 @@
+const texto1 = "Olá Mundo";
+const texto2 = 'Olá Mundo';
+const senha = 'senha23242';
+const StringDeNumeros = "6575";
+
+const citacao = "Meu nome é ";
+const meuNome = "Leonardo";
+console.log(citacao)
+
+// concatenação  (+)
+
+console.log(citacao + meuNome)
+
+//template string ou template literal
